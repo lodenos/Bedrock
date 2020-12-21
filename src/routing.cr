06ec3d846a12ef49e2d43a3de9cad9b2a13acafb
lodenos <nikolaiilodenos@gmail.com>
2020-12-21 22:35:21 +0000

    add query in parameter

--- src/routing.cr
+++ src/routing.cr
@@ -1,4 +1,5 @@
 require "http/server"
+require "uri"
 
 module Bedrock
   abstract class Routing
@@ -12,26 +13,24 @@ module Bedrock
     private def match_route(path : String)
       parameter = {} of String => String
       return yield parameter if path == "*"
-      pathRef = path.split('/')
-      path = @request.not_nil!.path.split('/')
+      pathRef = path.split '/'
+      path = @request.not_nil!.path.split '/'
       return if path.size != pathRef.size
       index = 0
       loop do
         return if index >= pathRef.size
         if pathRef[index].size == 0
-          index = index + 1
+          index += 1
           next
         end
         if pathRef[index][0] == ':'
           parameter[pathRef[index][1...]] = path[index]
-          if index >= path.size - 1
-            break
-          end
+          break if index >= path.size - 1
         elsif pathRef[index] != path[index]
           return
         end
         break if index >= pathRef.size - 1
-        index = index + 1
+        index += 1
       end
       yield parameter
       @pathFinded = true
@@ -50,10 +49,11 @@ module Bedrock
     {% begin %}
       {% methods = %w(CONNECT DELETE GET HEAD OPTIONS PATCH POST PUT TRACE) %}
       {% for method in methods %}
-        def {{method.downcase.id}}(path : String, &block)
-          return unless @request.not_nil!.method == {{method}}
+        def {{ method.downcase.id }}(path : String, &block)
+          return unless @request.not_nil!.method == {{ method }}
           return if self.path_finded?
           self.match_route path do |params|
+            params["query"] = URI.parse(path).query_params
             yield params
           end
         end
